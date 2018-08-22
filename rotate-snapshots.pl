fbbaaa5049b345300ebb635cc08ac71f3051b843
Colin TÃck <ct@tueck.de>
2018-08-23 00:25:29 +0200

    added chart and batch mode

--- rotate-snapshots.pl
+++ rotate-snapshots.pl
@@ -7,13 +7,11 @@ my @Existing;
 my %Keep;
 my @Intervals;
 my $Latest;
-my $Base = shift(@ARGV);
 my $TimestampFile = ".snapshot-timestamp";
+my $Base;
+my $Batch = 0;
 
--d $Base || die("'$Base' is not a directory.\n");
-
-unless ($Base =~ /\/$/) { $Base .= '/'; }
-
+# process arguments
 foreach(@ARGV) {
 	$_ = lc($_);
 	if (/^\s*(\d+)\s*x\s*(\d+)\s*([hdwmy]?)\s*$/) {
@@ -26,30 +24,49 @@ foreach(@ARGV) {
 		$interval *= 3600 * 24 * 30	if $unit eq 'm';
 		$interval *= 3600 * 24 * 365	if $unit eq 'y';
 		push(@Intervals, { "n" => $n, "interval" => $interval, "readable" => $2.$unit });
+		warn("Interval definition with n=1 does not make real sense.\n") if $n eq "1";
 	} elsif (/^\s*\+(\d+)\s*$/) {
 		$Latest = $1;
+	} elsif (/^\s*\-(b|\-batch)\s*$/) {
+		$Batch = 1;
+	} elsif (/^\s*(\/.*|.*\/)\s*$/) {
+		if ($Base != "") {
+			die("You should specify exactly one directory on which to operate.\n");
+		}
+		$Base = $1;
 	} else {
-		die("'$_' is not a valid interval definition.");
+		die("'$_' is not a valid option, interval definition or directory (should start or end with /).");
 	}
 }
 
-@Intervals = sort { $b->{"interval"} <=> $a->{"interval"} } @Intervals;
+# check if $Base is a directory
+-d $Base || die("'$Base' is not a directory.\n");
+
+unless ($Base =~ /\/$/) { $Base .= '/'; } # make sure we always have a trailing slash
+
+@Intervals = sort { $b->{"interval"} <=> $a->{"interval"} } @Intervals; # sort intervals from longest to shortest
+
+# check if we got sane parameters
+if ( ($#Intervals < 0) && ($Latest < 1) ) {
+	die("You need to specify at least one interval or a number of last snapshots to keep.\n");
+	# otherwise, everything will be deleted...
+}
 
 print "Rotating snapshots in $Base with following schedule:\n";
-print "* keeping the $Latest last snapshots\n" if ($Latest > 0);
+print " * keeping the $Latest last snapshots\n" if ($Latest > 0);
 foreach(@Intervals) {
-	print "* keeping (at least) one snapshot every ".$_->{readable}." up to ".$_->{n}." times\n";
+	print " * keeping up to ".$_->{n}." snapshots at ".$_->{readable}." intervals\n";
 }
 
 # find snapshots
 
 opendir(my $D, $Base) or die("Cannot open directory: $!");
-
 my @ReadDir = readdir $D;
 closedir $D;
 
 foreach (@ReadDir) {
 	if ( (! /^\.\.?$/) && (-f $Base.'/'.$_.'/'.$TimestampFile) ) {
+		# we only 
 		my $tag = $_;
 		my $mtime = (stat($Base.'/'.$tag.'/'.$TimestampFile))[9];
 		push(@Existing, { "tag" => $tag, "mtime" => $mtime });
@@ -60,13 +77,16 @@ foreach (@ReadDir) {
 
 for(my $i=0;$i<=$#Existing;$i++) {
 	$Existing[$i]->{i} = $i;
+	$Existing[$i]->{chart} = [ ] unless ($Batch);
 }
 
-print "\nCurrently, there are ".($#Existing+1)." snapshots.\n";
+print "\n".($#Existing+1)." snapshots found.\n";
 
 my $start = 1;
 
 my $lastmtime = 0;	# since we start with 0, the second element will (almost) always be beyond interval, and first one is thus added
+
+my $interval = 1 unless ($Batch);
 
 foreach (@Intervals) {
 
@@ -77,13 +97,17 @@ foreach (@Intervals) {
 	for(my $i=$start;$i<=$#Existing;$i++) {
 		if ($Existing[$i]->{mtime} > ($lastmtime + $_->{interval})) {
 			push(@List, $Existing[$i-1]);
+			$Existing[$i-1]->{chart}->[$interval] = 1 unless ($Batch);
 			##print (($i-1)."..".$Existing[$i-1]->{tag}." (".$Existing[$i]->{tag}." is more than ".$_->{interval}." after $lastmtime\n");
 			$lastmtime = $Existing[$i-1]->{mtime};
+		} else {
+			$Existing[$i-1]->{chart}->[$interval] = -1 unless ($Batch);
 		}
 	}
 	
 	# last one always added
 	push(@List, $Existing[$#Existing]);
+	$Existing[$#Existing]->{chart}->[$interval] = 1 unless ($Batch);
 
 	$start = ( $#List >= 2 ? $List[$#List-2]->{i} : 0 ) + 1;
 	$lastmtime = $Existing[$start-1]->{mtime};
@@ -94,35 +118,57 @@ foreach (@Intervals) {
 
 	foreach (@List) {
 		$Keep{$_->{tag}} = 1;
+		$_->{chart}->[$interval] = 2 unless ($Batch);
 	}
+
+	$interval++;
 }
 
 if ($Latest > 0) {
 	for(my $i=($#Existing-$Latest+1 > 0 ? $#Existing-$Latest+1 : 0);$i<=$#Existing;$i++) {
 		$Keep{$Existing[$i]->{tag}} = 1;
+		$Existing[$i]->{chart}->[0] = 2 unless ($Batch);
 	}
 }
 
-foreach (@Existing) {
-	print $_->{i};
-	if ($Keep{$_->{tag}}) {
-		print " --- ";
-	} else {
-		print " DEL ";
+unless ($Batch) {
+	print "\n";
+	foreach (@Existing) {
+		if ($Keep{$_->{tag}}) {
+			print " --> ";
+		} else {
+			print " xxx ";
+		}
+		print "[".$_->{i}."]".$_->{tag}."\t".$_->{mtime}."\t";
+		foreach(@{$_->{chart}}) {
+			print " o " if ($_ == 2);
+			print " . " if ($_ == 1);
+			print "   " if ($_ == 0);
+			print " | " if ($_ == -1);
+		}
+		print "\n";
 	}
-	print $_->{tag}."\n";
+
+	print "\nCommit changes? ";
+
+	$_ = <STDIN>; unless (/^[yY]/) {
+		die("Exiting.\n");
+	}
 }
 
-print "\nCommit changes? ";
-
-$_ = <STDIN>; unless (/^[yY]/) {
-	die("Exiting.\n");
-}
+my $delcount = 0;
 
 foreach (@Existing) {
 	unless ($Keep{$_->{tag}}) {
-		print "Deleting $Base".$_->{tag}."\n";
-		rmtree($Base.$_->{tag});
+		print("Deleting $Base".$_->{tag}.": ");
+		if (rmtree($Base.$_->{tag})) {
+			print("OK\n");
+			$delcount++;
+		} else {
+			die("Error while deleting - exiting.\n");
+		}
 	}
 }
+
+print "$delcount snapshots deleted.\n";
 
